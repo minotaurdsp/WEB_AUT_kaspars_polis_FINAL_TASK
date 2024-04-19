f6ec526b803566c7dd8f8b7ec39b2730cdf1255e
Kaspars Polis <kaspars.polis@printful.com>
2024-04-19 19:38:17 +0300

    Fill form / submit  / validate test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,12 +6,90 @@ describe('Elements', () => {
   context("Start testing", () => {
     
     // open page
-    beforeEach(() => {
-      Scene.visit();
+    beforeEach('Open base page',() => {
+      Form.visit();
     })
 
-    it('Practice Form', () => {
-      Form.fillForm();
+    it('Fil submit validate ', () => {
+      
+      // Set first name
+      Form.firstNameField.type('John');
+      Form.firstNameField.should('have.value', 'John')
+
+
+      // Set last name
+      Form.lastNameField.type('Doe');
+      Form.lastNameField.should('have.value', 'Doe')
+
+      
+      // Set email
+      Form.userEmailField.type('Jogn.Doe@magic.com');
+      Form.userEmailField.should('have.value', 'Jogn.Doe@magic.com')
+
+
+      // Set gender
+      Form.genderCheckbox.click();
+
+
+      // Set phone
+      Form.userNumberField.type('21150861');
+      Form.userNumberField.should('have.value', '21150861')
+
+      // Set Subjects to Economics.
+      cy.get('#subjectsInput').type('Economics')
+      cy.get('#subjectsInput').click().focused().type('{downarrow}{enter}',{force:true});
+      cy.get('.subjects-auto-complete__multi-value__label').should(
+          "have.text",
+          'Economics'
+        );
+
+        
+      // c. Set the - Date of Birth - with Calendar widget to - 28th of February, 1930.
+      cy.get('#dateOfBirthInput').click();
+      // Select year
+      cy.get('.react-datepicker__year-select').select('1930');
+      // Select month
+      cy.get('.react-datepicker__month-select').select('March');
+      // Select day
+      cy.get(`.react-datepicker__day--0${28}`).first().click();
+      // Assert input date value
+      cy.get('#dateOfBirthInput').should('have.value', '28 Feb 1930');
+
+      // e. Set Hobbies to Music.
+      Form.hobbiesCheckbox.click();
+
+
+      // select file
+      Form.pictureFile.selectFile('cypress/files/one-funny-dog-isolated-white-background-png.jpg')
+
+
+      // g. Set State to NCR.
+      cy.get('#state').type('NCR');
+      cy.get('#state').click().focused().type('{downarrow}{enter}',{force:true});
+      cy.get('#state > div > div.css-1hwfws3 > div.css-1uccc91-singleValue').should(
+          'have.text',
+          'NCR'
+      );
+
+      // h. Set City to Delhi.
+      cy.get('#city').type('Delhi');
+      cy.get('#city').click().focused().type('{downarrow}{enter}',{force:true});
+      cy.get('#city > div > div.css-1hwfws3 > div.css-1uccc91-singleValue').should(
+          'have.text',
+          'Delhi'
+      );
+
+
+      Form.clickSubmitButton.click()
+
+      // j. Validate that each Labeled row contains the correct information.
+      Form.validateForm();
+      
+      // Close modal
+      Form.closeModal.click({force: true});
+
     })
+
+
   })
 })
--- cypress/pageObjects/Form.page.js
+++ cypress/pageObjects/Form.page.js
@@ -3,34 +3,110 @@ import BasePage from "./base.page";
 class Form extends BasePage {
     
     static get clickSubmitButton() {
-        return cy.get("#btn-make-appointment");
+        return cy.get("#submit");
+    }
+    
+    static get closeModal() {
+        return cy.get('#closeLargeModal');
+        
     }
 
-    static fillForm() {  
-        cy.get('#firstName').type('John');
-        
-        cy.get('#lastName').type('Doe');
-        
-        cy.get('#userEmail').type('Jogn.Doe@magic.com');
-        
-        cy.get('#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(1)').click();
-        
-        cy.get('#userNumber').type('+371 21150861');
+    static get firstNameField() {
+        return cy.get('#firstName');
+    }
 
-        // c. Set the - Date of Birth - with Calendar widget to - 28th of February, 1930.
-        //cy.get('#dateOfBirthInput').click();
+    static get lastNameField() {
+        return cy.get('#lastName');
+    }
 
-        // Set Subjects to Economics.
-        cy.get('#subjectsInput').type('Economics')
-        cy.get('#subjectsInput').click().focused().type('{downarrow}{enter}',{force:true});
-        
-        // e. Set Hobbies to Music.
-        cy.get('#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(2)').click();
-    };
+    static get userEmailField() {
+        return cy.get('#userEmail');
+    }
 
-    
+    static get genderCheckbox() {
+        return cy.get('#genterWrapper > div.col-md-9.col-sm-12 > div:nth-child(3)');
+    }
 
-  
+    static get userEmailField() {
+        return cy.get('#userEmail');
+    }
+
+    static get userNumberField() {
+        return cy.get('#userNumber');
+    }
+
+
+    static get hobbiesCheckbox() {
+        return cy.get('#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(3)');
+    }
+
+    static get pictureFile() {
+        return cy.get('#uploadPicture');
+    }
+
+
+
+    static validateForm() {
+        // Validate Student  name
+        cy.get('.modal-body > div > table > tbody > tr:nth-child(1) > td:nth-child(2)').should(
+            'have.text',
+            'John Doe'
+        );
+
+        // Validate  Email
+        cy.get('.modal-body > div > table > tbody > tr:nth-child(2) > td:nth-child(2)').should(
+            'have.text',
+            'Jogn.Doe@magic.com'
+        );
+
+        // Validate  Gender
+        cy.get('.modal-body > div > table > tbody > tr:nth-child(3) > td:nth-child(2)').should(
+            'have.text',
+            'Other'
+        );
+
+        // Validate  Phone
+        cy.get('.modal-body > div > table > tbody > tr:nth-child(4) > td:nth-child(2)').should(
+            'have.text',
+            '21150861'
+        );
+
+        // Validate Date of Birth
+        cy.get('.modal-body > div > table > tbody > tr:nth-child(5) > td:nth-child(2)').should(
+            'have.text',
+            '28 February,1930'
+        );
+
+        // Validate Subjects
+        cy.get('.modal-body > div > table > tbody > tr:nth-child(6) > td:nth-child(2)').should(
+            'have.text',
+            'Economics'
+        );
+
+        // Validate Hobbies
+        cy.get('.modal-body > div > table > tbody > tr:nth-child(7) > td:nth-child(2)').should(
+            'have.text',
+            'Music'
+        );
+
+        // Validate Picture
+        cy.get('.modal-body > div > table > tbody > tr:nth-child(8) > td:nth-child(2)').should(
+            'have.text',
+            'one-funny-dog-isolated-white-background-png.jpg'
+        );
+
+        // Validate Address
+        cy.get('.modal-body > div > table > tbody > tr:nth-child(9) > td:nth-child(2)').should(
+            'have.text',
+            ''
+        );
+
+        // Validate State and City
+        cy.get('.modal-body > div > table > tbody > tr:nth-child(10) > td:nth-child(2)').should(
+            'have.text',
+            'NCR Delhi'
+        );
+    }
 }
   
 export default Form;
